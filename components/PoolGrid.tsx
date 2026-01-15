ecf299269f6992fa61c6e1c23068234ae38c737b
Jason Stephens <jasonpaulstephens@gmail.com>
2026-01-15 16:58:50 -0500

    Make header row and column the same size

--- components/PoolGrid.tsx
+++ components/PoolGrid.tsx
@@ -196,8 +196,8 @@ export default function PoolGrid({
 
   // Cell size - larger for desktop
   const cellSize = 'w-[52px] h-[52px] sm:w-[62px] sm:h-[62px] md:w-[72px] md:h-[72px] lg:w-[82px] lg:h-[82px]';
-  const headerSize = 'w-[52px] h-[44px] sm:w-[62px] sm:h-[50px] md:w-[72px] md:h-[56px] lg:w-[82px] lg:h-[60px]';
-  const rowHeaderSize = 'w-[44px] h-[52px] sm:w-[50px] sm:h-[62px] md:w-[56px] md:h-[72px] lg:w-[60px] lg:h-[82px]';
+  const headerSize = 'w-[52px] h-[40px] sm:w-[62px] sm:h-[44px] md:w-[72px] md:h-[48px] lg:w-[82px] lg:h-[52px]';
+  const rowHeaderSize = 'w-[40px] h-[52px] sm:w-[44px] sm:h-[62px] md:w-[48px] md:h-[72px] lg:w-[52px] lg:h-[82px]';
 
   return (
     <div className="w-full flex flex-col items-center overflow-x-auto">
